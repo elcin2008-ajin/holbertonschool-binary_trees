3d1becf7f95777332dd96eb947910f4ad3f316bf
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-27 14:18:51 +0000

    Kk

--- binary_trees.h
+++ binary_trees.h
@@ -3,16 +3,23 @@
 
 #include <stddef.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 
-#endif
+#endif /* BINARY_TREES_H */
