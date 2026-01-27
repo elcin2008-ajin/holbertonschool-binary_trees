a5b2020743d48d2e222f2b900111fe06c0810241
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-27 14:14:19 +0000

    kk

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
 typedef struct binary_tree_s
 {
     int n;
@@ -5,3 +10,9 @@ typedef struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 } binary_tree_t;
+
+/* Function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif
