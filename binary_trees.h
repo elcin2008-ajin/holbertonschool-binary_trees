aba398e097475d25aaf2d16c0fee13e136030946
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-30 12:24:21 +0000

    kk

--- binary_trees.h
+++ binary_trees.h
@@ -13,18 +13,29 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
+/* Basic binary tree functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
+/* Task 4 */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
