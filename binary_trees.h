03ba614dfa1a79a71642d7d50b366dae16e47b46
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-30 12:11:44 +0000

    kk

--- binary_trees.h
+++ binary_trees.h
@@ -1,25 +1,33 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-/* Structure for a binary tree node */
-typedef struct binary_tree_s
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} binary_tree_t;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
-/* Prototypes for tasks 0 - 3 */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Basic binary tree functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-
-/* Provided function to print the binary tree */
 void binary_tree_print(const binary_tree_t *tree);
+
+/* Task 4 */
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
