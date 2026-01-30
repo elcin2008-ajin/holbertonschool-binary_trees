760968eda9ae3749c7241f1221e2a157805bf2f5
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-30 12:19:38 +0000

    kk

--- binary_trees.h
+++ binary_trees.h
@@ -30,4 +30,7 @@ void binary_tree_print(const binary_tree_t *tree);
 /* Task 4 */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* Task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
