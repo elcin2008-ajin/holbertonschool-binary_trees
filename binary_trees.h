71dd444f6b1e0f644c1a398475385d18023cc721
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-30 12:29:51 +0000

    kk

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* Task 8 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
+
 #endif /* BINARY_TREES_H */
