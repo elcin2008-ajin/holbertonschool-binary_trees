fb4fc12441efce7ddc7076d78a93d4a1f733b8e0
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-01-30 12:52:37 +0000

    Kk

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,17 +8,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* leaf */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	/* leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* both children must exist */
-    if (tree->left != NULL && tree->right != NULL)
-        return (binary_tree_is_full(tree->left) &&
-                binary_tree_is_full(tree->right));
+	/* both children must exist */
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
 
-    return (0);
+	return (0);
 }
